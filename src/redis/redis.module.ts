02ee6ab2ccad3ca9bcf43c9555f54f48f0c6a1f0
Amr Mubarak <amrrdev@gmail.com>
2025-02-15 16:19:54 +0200

    feat(redis): add Redis module with configuration and service

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import redisConfig from './config/redis.config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import Redis from 'ioredis';
+import { RedisService } from './redis.service';
+
+@Module({
+  imports: [ConfigModule.forFeature(redisConfig)],
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: (redisConfigrations: ConfigType<typeof redisConfig>) => {
+        return new Redis({
+          port: parseInt(redisConfigrations.port ?? '6379', 10),
+          host: redisConfigrations.host,
+        });
+      },
+      inject: [redisConfig.KEY],
+    },
+    RedisService,
+  ],
+  exports: ['REDIS_CLIENT', RedisService],
+})
+export class RedisModule {}
