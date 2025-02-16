0857554751fe496f398834a9ba3f14fc25c594ff
Amr Mubarak <amrrdev@gmail.com>
2025-02-17 00:43:32 +0200

    chore: integrate OpenAiModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './redis/redis.module';
+import { UsersModule } from './users/users.module';
+import { OpenAiModule } from './integrations/open-ai/open-ai.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    AuthModule,
+    RedisModule,
+    UsersModule,
+    OpenAiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
