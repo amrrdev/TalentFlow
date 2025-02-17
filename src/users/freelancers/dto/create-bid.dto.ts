d5d7e5a5ede7d464ef94febc1ba8fb8a8e609aa8
Amr Mubarak <amrrdev@gmail.com>
2025-02-18 00:29:29 +0200

    feat: Add CreateBidDto with validation for bid amount, estimated work, cover letter, CV, and experience.

--- src/users/freelancers/dto/create-bid.dto.ts
+++ src/users/freelancers/dto/create-bid.dto.ts
@@ -1,15 +1,22 @@
-import { IsNumber, IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
 
 export class CreateBidDto {
   @IsNumber()
   amount: number;
 
   @IsString()
+  @IsNotEmpty()
   estimatedWork: string;
 
   @IsString()
+  @IsNotEmpty()
   coverLetter: string;
 
   @IsUrl()
+  @IsNotEmpty()
   cv: string;
+
+  @IsString()
+  @IsNotEmpty()
+  experience: string;
 }
