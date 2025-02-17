859acb2e6c4604d6436a08772f3fbb9db50c33d7
Amr Mubarak <amrrdev@gmail.com>
2025-02-18 00:31:43 +0200

    feat: Add EmailType enum to define various email types for sending notifications

---
+++ src/integrations/mail/enums/email-type.enum.ts
@@ -0,0 +1,7 @@
+export enum EmailType {
+  AcceptedBid = 'AcceptedBid',
+  VerifyEmail = 'VerifyEmail',
+  RejectedBid = 'RejectedBid',
+  AcceptedProposal = 'AcceptedProposal',
+  RejectedProposal = 'rejectedProposal',
+}
