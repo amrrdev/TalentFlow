0382ee649561dbb5e61e935423bf5b697a60a6a0
Amr Mubarak <amrrdev@gmail.com>
2025-02-19 00:23:16 +0200

    chore(queue): register mail, contract, and chat queues with BullModule

--- src/integrations/queue/queue.module.ts
+++ src/integrations/queue/queue.module.ts
@@ -5,11 +5,18 @@ import { RedisModule } from '../../redis/redis.module';
 import { MailProcessor } from '../mail/mail.processor';
 import { ConfigModule } from '@nestjs/config';
 import mailConfig from '../mail/config/mail.config';
+import { ContractProcessor } from '../../users/clients/proposal/contract.processor';
+import { DatabaseModule } from '../../database/database.module';
+import { ChatProcessor } from './chat.processor';
+import { ChatModule } from '../../chat/chat.module';
 
 @Module({
   imports: [
+    ChatModule,
+    DatabaseModule,
     ConfigModule.forFeature(mailConfig),
     RedisModule,
+    ChatModule,
     BullModule.forRootAsync({
       imports: [RedisModule],
       useFactory: (redisClient: Redis) => ({
@@ -18,11 +25,19 @@ import mailConfig from '../mail/config/mail.config';
       inject: ['REDIS_CLIENT'],
     }),
 
-    BullModule.registerQueue({
-      name: 'mail-queue',
-    }),
+    BullModule.registerQueue(
+      {
+        name: 'mail-queue',
+      },
+      {
+        name: 'contract-queue',
+      },
+      {
+        name: 'chat-queue',
+      },
+    ),
   ],
-  providers: [MailProcessor],
+  providers: [MailProcessor, ContractProcessor, ChatProcessor],
   exports: [BullModule],
 })
 export class QueueModule {}
