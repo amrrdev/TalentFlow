045a2b1c17696414cc1971ef43ebbb8849b56026
Amr Mubarak <amrrdev@gmail.com>
2025-02-19 16:43:29 +0200

    feat: add ChatModule to UsersModule imports for chat functionality.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -11,9 +11,10 @@ import { ProposalController } from './freelancers/proposals/proposal.controller'
 import { ClientProposalService } from './clients/proposal/client-proposal.service';
 import { ClientProposalController } from './clients/proposal/client-proposal.controller';
 import { QueueModule } from '../integrations/queue/queue.module';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
-  imports: [DatabaseModule, MailModule, GroqModule, QueueModule],
+  imports: [DatabaseModule, MailModule, GroqModule, QueueModule, ChatModule],
   providers: [FreelancersService, ClientsService, ProposalService, ClientProposalService],
   controllers: [
     FreelancersController,
