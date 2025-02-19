5a72191756a06b4e66dc96afa459ce971aafab95
Amr Mubarak <amrrdev@gmail.com>
2025-02-19 16:39:17 +0200

    Implement chat ID retrieval and message saving functionality.

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { DatabaseService } from '../database/database.service';
 
 @Injectable()
@@ -16,7 +16,7 @@ export class ChatService {
     });
 
     if (chatExists) {
-      return chatExists;
+      return chatExists.id;
     }
     const newChat = await this.databaseService.chat.create({
       data: {
@@ -24,6 +24,48 @@ export class ChatService {
         freelancerId,
       },
     });
-    return newChat;
+    return newChat.id;
+  }
+
+  async getChatId(clientId: number, proposalId: number) {
+    const proposal = await this.databaseService.proposals.findUnique({
+      where: {
+        id: proposalId,
+        status: 'accepted',
+      },
+    });
+
+    if (!proposal) {
+      throw new BadRequestException(
+        'You must accept the proposal first before starting a chat with the freelancer.',
+      );
+    }
+
+    const chat = await this.databaseService.chat.findUnique({
+      where: {
+        clientId_freelancerId: {
+          freelancerId: proposal.freelancerId,
+          clientId,
+        },
+      },
+    });
+
+    if (!chat) {
+      throw new BadRequestException(
+        'No chat found. You must accept the proposal to establish a chat.',
+      );
+    }
+    return { chatId: chat.id };
+  }
+
+  async saveMessages(clientId: number, chatId: number, message: string) {
+    await this.databaseService.messages.create({
+      data: {
+        content: message,
+        chatId: chatId,
+        senderId: clientId,
+      },
+    });
+    console.log('message saved');
   }
 }
