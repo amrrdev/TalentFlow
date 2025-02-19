85c91629e0897c1e2df5ceabb21ee7d0560cef3d
Amr Mubarak <amrrdev@gmail.com>
2025-02-19 16:39:37 +0200

    Add ChatService to providers and exports in ChatModule.

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
-import { ChatGateway } from './chat.gateway';
+import { forwardRef, Global, Module } from '@nestjs/common';
+import { ChatGateway } from './gateway/chat.gateway';
 import { ChatService } from './chat.service';
 import { DatabaseModule } from '../database/database.module';
 import { AuthModule } from '../auth/auth.module';
@@ -7,15 +7,15 @@ import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from '../auth/config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 
+@Global()
 @Module({
   imports: [
     DatabaseModule,
     AuthModule,
     JwtModule.registerAsync(jwtConfig.asProvider()),
-
     ConfigModule.forFeature(jwtConfig),
   ],
-  providers: [ChatGateway, ChatService],
+  providers: [ChatService],
   exports: [ChatService],
 })
 export class ChatModule {}
